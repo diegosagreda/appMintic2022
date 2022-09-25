a83287670a1c6a1079d90a65d73634dbd88967f4
diegosagreda <diegoagreda1996@gmail.com>
2022-09-24 22:26:12 -0500

    Despliegue 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useState, useContext, useEffect } from 'react'
+import { useContext } from 'react'
 import { AppContext } from './context/AppContex'
 import './App.css'
 import Home from './components/home/Home'
 import Login from './components/login/Login';
-import Recargo from './components/recargo/recargo'
+/* import Recargo from './components/recargo/recargo' */
 
 
 function App() {
@@ -23,7 +23,7 @@ function App() {
         <Routes>  
           <Route exact path='/' element={<Login/>}/>        
           <Route exact path='/login' element={<Login/>}/>   
-          <Route path='*' element={<Recargo/>}/>         
+        {/*   <Route path='*' element={<Recargo/>}/>      */}    
         </Routes>
       </BrowserRouter>   
     }
